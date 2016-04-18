d479e60a09625dbc12eaa04210cc4f7f2529d9a7
Peter Daily <thepeted@gmail.com>
2016-04-18 17:37:46 +0100

    add reduix-thunk

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 import reducers from './reducers';
 
 import createLevel from './bin/map-creator';
