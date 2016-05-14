aa7f1e798655d3ac2c9a28eda0d3f2c76c453907
Peter Daily <thepeted@gmail.com>
2016-05-14 10:35:58 +0100

    tidy game component, add viewport settings to constructor, commenting

--- src/containers/game.js
+++ src/containers/game.js
@@ -5,8 +5,6 @@ import { connect } from 'react-redux';
 import playerInput, { createLevel, toggleFogMode, setDungeonLevel } from '../actions/';
 import Cell from '../components/cell';
 
-const V_OFFSET = 5; // in ems to match any elements above component in page layout
-
 class grid extends Component {
 	constructor() {
 		super();
@@ -14,68 +12,32 @@ class grid extends Component {
 			viewportWidth: 0,
 			viewportHeight: 0
 		};
+
 		this.handleKeyPress = this.handleKeyPress.bind(this);
 		this.handleResize = this.handleResize.bind(this);
+
+		this.VP_HEIGHT_OFFSET = 4; // in ems to match elements above this component
+		this.VP_MINIMUM_HEIGHT = 15 // in ems
+		// set ratios for determining the viewport size
+		this.VP_WIDTH_RATIO = 30
+		this.VP_HEIGHT_RATIO = 21
 	}
 
 	componentWillMount() {
-		const viewportWidth = window.innerWidth / 30;
-		const viewportHeight = Math.max(15, (window.innerHeight / 21) - V_OFFSET);
+		//set the initial veiwport size
+		const viewportWidth = window.innerWidth / this.VP_WIDTH_RATIO;
+		const viewportHeight = Math.max(
+			this.VP_MINIMUM_HEIGHT,
+			(window.innerHeight / this.VP_HEIGHT_RATIO) - this.VP_HEIGHT_OFFSET
+		);
+		this.setState({ viewportWidth, viewportHeight });
 		this.props.createLevel();
 		this.props.setDungeonLevel(1);
-		this.setState({ viewportWidth, viewportHeight });
 	}
 
 	componentDidMount() {
 		window.addEventListener('keydown', _.throttle(this.handleKeyPress, 100));
 		window.addEventListener('resize', _.debounce(this.handleResize, 500));
-	}
-
-	render() {
-		// must be divisible by 2
-		const viewportHeight = this.state.viewportHeight - this.state.viewportHeight % 2;
-		const viewportWidth = this.state.viewportWidth - this.state.viewportWidth % 2;
-		const { entities } = this.props.grid;
-		const [ playerX, playerY ] = this.props.grid.playerPosition;
-
-		const top = _.clamp(playerY - viewportHeight / 2, 0, entities.length - viewportHeight);
-		const right = Math.max(playerX + viewportWidth / 2, viewportWidth);
-		const bottom = Math.max(playerY + viewportHeight / 2, viewportHeight);
-		const left = _.clamp(playerX - viewportWidth / 2, 0, entities[0].length - viewportWidth);
-
-		const newEntities = entities.map((row, i) => row.map((cell, j) => {
-			cell.distanceFromPlayer = (Math.abs(playerY - i)) + (Math.abs(playerX - j));
-			return cell;
-		}));
-
-		const cells = newEntities.filter((row, i) => i >= top && i <= bottom)
-		.map((row, i) => {
-			return (
-				<div key={i} className="row">
-					{
-						row
-						.filter((row, i) => i >= left && i <= right)
-						.map((cell, j) => {
-							return (
-								<Cell
-									key={j}
-									cell={cell}
-									distance={cell.distanceFromPlayer}
-									zone={this.props.grid.dungeonLevel}
-									visible={this.props.fogMode}
-									/>
-							);
-						})
-					}
-				</div>
-			);
-		});
-
-		return (
-			<div className="grid-wrapper">
-					{cells}
-			</div>
-		);
 	}
 
 	handleKeyPress(e) {
@@ -108,9 +70,64 @@ class grid extends Component {
 	}
 
 	handleResize(e) {
-		const viewportWidth = e.target.innerWidth / 30;
-		const viewportHeight = Math.max(15, (e.target.innerHeight / 21) - V_OFFSET);
+		const viewportWidth = e.target.innerWidth / this.VP_WIDTH_RATIO;
+		const viewportHeight = Math.max(
+			this.VP_MINIMUM_HEIGHT,
+			(e.target.innerHeight / this.VP_HEIGHT_RATIO) - this.VP_HEIGHT_OFFSET
+		);
 		this.setState({ viewportWidth, viewportHeight });
+	}
+
+	render() {
+		// ensure the viewport height and width is always even
+		const viewportHeight = this.state.viewportHeight - this.state.viewportHeight % 2;
+		const viewportWidth = this.state.viewportWidth - this.state.viewportWidth % 2;
+
+		const { entities } = this.props.grid;
+		const [ playerX, playerY ] = this.props.grid.playerPosition;
+
+		// define the limits of the cells to be displayed in the viewport
+		const top = _.clamp(playerY - viewportHeight / 2, 0, entities.length - viewportHeight);
+		const right = Math.max(playerX + viewportWidth / 2, viewportWidth);
+		const bottom = Math.max(playerY + viewportHeight / 2, viewportHeight);
+		const left = _.clamp(playerX - viewportWidth / 2, 0, entities[0].length - viewportWidth);
+
+		// create a new array of entities which includes the distance from the player
+		// used to enable fog mode
+		const newEntities = entities.map((row, i) => row.map((cell, j) => {
+			cell.distanceFromPlayer = (Math.abs(playerY - i)) + (Math.abs(playerX - j));
+			return cell;
+		}));
+
+		// create cell components from the entities that are in scope of the viewport
+		const cells = newEntities.filter((row, i) => i >= top && i < bottom)
+		.map((row, i) => {
+			return (
+				<div key={i} className="row">
+					{
+						row
+						.filter((row, i) => i >= left && i < right)
+						.map((cell, j) => {
+							return (
+								<Cell
+									key={j}
+									cell={cell}
+									distance={cell.distanceFromPlayer}
+									zone={this.props.grid.dungeonLevel}
+									visible={this.props.fogMode}
+									/>
+							);
+						})
+					}
+				</div>
+			);
+		});
+
+		return (
+			<div className="grid-wrapper">
+					{cells}
+			</div>
+		);
 	}
 }
 
